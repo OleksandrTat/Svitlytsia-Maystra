4f7db73e386b4d10faacd91b727e1f08eddffe35
OleksandrTat <ifpoleksandrtataryn@gmail.com>
2025-01-02 21:24:12 +0100

    Закінчив блок замовити

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault(); // Відміна стандартної поведінки
+
+        // Знаходимо цільовий елемент (секцію)
+        const targetElement = document.querySelector(this.getAttribute("href"));
+
+        // Встановлюємо відступ для фіксованого навбару
+        const navbarHeight = document.querySelector('.navbar').offsetHeight; // Висота навбару
+        const offset = -navbarHeight*200; // Коригуємо відступ по висоті навбару
+
+        // Скролимо до цільового елемента з відступом
+        window.scrollTo({
+            top: targetElement.offsetTop + offset, // Використовуємо offset для коригування позиції
+            behavior: 'smooth' // Плавна прокрутка
+        });
+    });
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('a[href="#about-us"]').addEventListener('click', function (event) {
         event.preventDefault(); // Запобігає стандартній прокрутці
@@ -10,4 +31,18 @@ document.addEventListener('DOMContentLoaded', function () {
         behavior: 'smooth', // Плавна прокрутка
         });
     });
-}); // скролить на середину блоку
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('a[href="#comprar"]').addEventListener('click', function (event) {
+        event.preventDefault(); // Запобігає стандартній прокрутці
+
+        const targetElement = document.querySelector('#comprar');
+        const offset = window.innerHeight / 2 - targetElement.offsetHeight / 2 * 1.2;
+
+        window.scrollTo({
+        top: targetElement.offsetTop - offset,
+        behavior: 'smooth', // Плавна прокрутка
+        });
+    });
+});
